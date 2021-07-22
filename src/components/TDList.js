58812a27dae387546f1e00c2bb9a9296364a72b5
karvus-hn <61095185+karvus-hn@users.noreply.github.com>
2021-07-22 13:39:33 +0300

    DELETE,POST,PUT запросы на сервер
    и соответствующая часть в интерфейсе

--- src/components/TDList.js
+++ src/components/TDList.js
@@ -6,9 +6,13 @@ class TDList extends React.Component
 {
 	state={ 
 		todos:[],
+		editElem:[],
 		ntodo:"",
 		userInfo:0,
-		dataIsReturned : false
+		dataIsReturned : false,
+		showDel:false,
+		showEdit:false,
+		delElem:''
 	};
 
 	componentWillMount = async () => 
@@ -27,21 +31,33 @@ class TDList extends React.Component
 		this.setState({ ntodo: e.target.value });
 	}
 
+	// Обработка кнопки "Добавить" - добавление нового дела, POST запрос на сервер
 	handleSubmit = (e) => 
 	{
 		e.preventDefault();
 		if (this.state.ntodo.length == 0) {
 			return;
 		}
-
-		const newTODO = { title: this.state.ntodo, id: Date.now()};
-		this.setState(state => ({
-			todos: state.todos.concat(newTODO),
-			ntodo: ''
-		}));
-		console.log(this.state.todos);
+		
+		const srvTodo= {userId:this.state.userInfo["id"], title:this.state.ntodo};
+		fetch(urlTODOs,
+			{
+				method : 'POST',
+				body : JSON.stringify(srvTodo),
+				headers: {'Content-Type': 'application/json'}
+			})
+		.then((response)=>response.json())
+		.then((json)=>
+		{	
+			const newLocalTODO = { title: this.state.ntodo, id: json["id"]};
+			this.setState(state => ({
+				todos: state.todos.concat(newLocalTODO),
+				ntodo: ''
+			}));
+		});
 	}
 
+	// Обработка нажатия на текст todo в списке - смена состояния, PUT запрос на сервер с 1 параметром
 	handleClick = (e) =>
 	{
 		e.preventDefault();
@@ -49,18 +65,26 @@ class TDList extends React.Component
 		let todos=this.state.todos;
 		let ind=todos.findIndex(x=>x.id==e.target.id);
 		todos[ind].completed=!todos[ind].completed;
+		fetch(urlTODOs,
+			{
+				method : 'PUT',
+				body : JSON.stringify({id:e.target.id}),
+				headers: {'Content-Type': 'application/json'}
+			})
 		this.setState({todos});
 	}
-
+	
+	// Обработка крестика - подсветка удаляемого элемента и флажок окна удаления
 	handleRemove = (e) =>
 	{
 		e.preventDefault();
-		let todos=this.state.todos;
-		let ind=todos.findIndex(x=>x.id==e.target.parentNode.children[0].id);
-		todos.splice(ind,1);
-		this.setState({todos});
+		console.log(e.target);
+		let delElem=e.target.parentNode;
+		delElem.style.backgroundColor='red';
+		this.setState({showDel:true,delElem:delElem});
 	}
 
+	// Выход пользователя
 	handleExit = (e) =>
 	{
 		e.preventDefault();
@@ -69,7 +93,78 @@ class TDList extends React.Component
 		localStorage['user']=-1;
 		this.props.handleLoginClick(false,-1);
 	}
-
+	
+	// Обработка подверждения в окне удаления - DELETE запрос и отключение подстветки, снятие флажка окна удаления
+	handleDel = (e) =>
+	{
+		e.preventDefault();
+		this.state.delElem.style.backgroundColor='';
+		let todos=this.state.todos;
+		let idDel=this.state.delElem.children[0].id;
+		let ind=todos.findIndex(x=>x.id==idDel);
+		fetch(urlTODOs,
+			{
+				method : 'DELETE',
+				body : JSON.stringify({id:idDel}),
+				headers: {'Content-Type': 'application/json'}
+			}) 
+		todos.splice(ind,1);
+		this.setState({todos:todos,showDel:false,delElem:''});
+	}
+	
+	// Обработки отмены в окне удаления - отключение подстветки, снятие флажка окна удаления
+	handleCancel = (e) =>
+	{
+		e.preventDefault();
+		this.state.delElem.style.backgroundColor='';
+		this.setState({showDel:false,delElem:''});
+	}
+	
+	// Обработка карандашика - флажок окна изменения
+	handleEditForm = (e) =>
+	{
+		e.preventDefault();
+		let todos=this.state.todos;
+		let idEdit=e.target.parentNode.children[0].id;
+		let ind=todos.findIndex(x=>x.id==idEdit);
+		this.setState({showEdit:true,editElem:{...this.state.todos[ind]}});
+	}
+	
+	handleChangeEF1 = (e) => {
+		let ee = {...this.state.editElem};
+		ee["title"] = e.target.value;
+		this.setState({ editElem: ee });
+	}
+	
+	handleChangeEF2 = (e) => {
+		let ee = {...this.state.editElem};
+		ee["completed"] =e.target.checked;
+		console.log(ee);
+		this.setState({ editElem: ee });
+	}
+	
+	// Обработка подверждения в окне изменения - PUT запрос на сервер для изменения 2 столбцов, снятие флажка окна изменения
+	handleSubmitEdit = (e) =>
+	{
+		e.preventDefault();
+		let todos=this.state.todos;
+		let ind=todos.findIndex(x=>x.id==this.state.editElem["id"]);
+		todos[ind]={...this.state.editElem};
+		fetch(urlTODOs,
+			{
+				method : 'PUT',
+				body : JSON.stringify(this.state.editElem),
+				headers: {'Content-Type': 'application/json'}
+			})
+		this.setState({showEdit:false,editElem:[],todos:todos});
+	}
+	
+	handleCancelEdit = (e) =>
+	{
+		e.preventDefault();
+		this.setState({showEdit:false});
+	}
+	
 	render()
 	{
 		return (
@@ -89,6 +184,7 @@ class TDList extends React.Component
 					(item) => 
 						<li onClick={this.handleClick}> 
 							<a id={item.id} class={item.completed?'cmpl':''} href=''> {item.title} </a>
+							<button class='edit' onClick={this.handleEditForm}></button>
 							<button class='del' onClick={this.handleRemove}></button>
 						</li>
 						)
@@ -97,8 +193,23 @@ class TDList extends React.Component
 			</div>
 		:<h1>Загрузка</h1>
 		}
+		{this.state.showDel? <div class='modal delete'> <h2> Удалить?</h2> <button onClick={this.handleDel}>Да</button> <button onClick={this.handleCancel}>Нет</button> </div>:''}
+		{this.state.showEdit? <div class='modal editForm'> 
+								<h2> Редактировать</h2> 
+								<div>
+									<label> Содержимое </label>
+									<input type="text" value={this.state.editElem["title"]} onChange={this.handleChangeEF1}/>
+								</div>
+								<div> 
+									<label> Завершено </label>
+									<input type="checkbox" value={this.state.editElem["completed"]} checked={this.state.editElem["completed"]} onChange={this.handleChangeEF2}/>
+								</div>
+								<button onClick={this.handleSubmitEdit}>Изменить</button> 
+								<button onClick={this.handleCancelEdit}>Отменить</button> 
+								</div>
+							:''}
 		</>
-		);
+		); // Возможно стоит выделить форму редактирования в новый компонент
 	}
 };
 
--- src/server/server.js
+++ src/server/server.js
@@ -11,18 +11,25 @@ let db = new sqlite3.Database(dbFileName, sqlite3.OPEN_READWRITE, (err) => {
   console.log('Connected to the tododb database.');
 });
 
-let queryU= 'SELECT * FROM user WHERE user.username= ? AND user.website = ?'
-let queryT= 'SELECT * FROM todo WHERE todo.userId= ? ORDER BY todo.id'
+let queryU = 'SELECT * FROM user WHERE user.username= ? AND user.website = ?'
+let queryT = 'SELECT * FROM todo WHERE todo.userId= ? ORDER BY todo.id'
 
-let queryUA= 'SELECT * FROM user'
-let queryTA= 'SELECT * FROM todo'
+let queryUAll = 'SELECT * FROM user'
+let queryTAll = 'SELECT * FROM todo'
+
+let queryTInsert = 'INSERT INTO todo VALUES ((SELECT max(todo.id) from todo)+1,(?),(?),0)';
+let queryTDelete = 'DELETE FROM todo WHERE todo.id= ? ';
+
+let queryTUpdate1 = 'UPDATE todo SET completed=NOT completed WHERE todo.id= ?';
+let queryTUpdate2 = 'UPDATE todo SET completed=(?), title = (?) WHERE todo.id= (?)';
 
 var app = express();
 app.use(cors());
 
-// Если запросы без параметров, то результат вся таблица, иначе часть
+const jsonParser = express.json();
+
+// Если GET-запросы без параметров, то результат вся таблица, иначе часть
 app.get('/users',function(request,response) {
-	var fileName = path.resolve(__dirname,'./data/users.json');
 	if ((typeof request.query.username=="undefined") && (typeof request.query.website=="undefined"))
 		db.all(queryUA,[],(err, rows)=>{
 		  if (err) {
@@ -40,21 +47,64 @@ app.get('/users',function(request,response) {
 });
 
 app.get('/todos',function(request,response) {
-	var fileName = path.resolve(__dirname,'./data/todos.json');
+	var sQuery = '';
+	
 	if (typeof request.query.userId=="undefined")
-		db.all(queryTA,[], (err, rows)=>{
-		  if (err) {
-			return console.error(err.message);
-		  }
-		  response.json(rows)
-		})
+		sQuery = queryTAll;
 	else
-		db.all(queryT,[request.query.userId], (err, rows)=>{
-		  if (err) {
-			return console.error(err.message);
-		  }
-		  response.json(rows)
-		})
+		sQuery = queryT;
+	
+	db.all(sQuery,[request.query.userId], (err, rows)=>{
+	  if (err) {
+		return console.error(err.message);
+	  }
+	  response.json(rows)
+	})
+});
+
+// Создание todo
+app.post('/todos',jsonParser,function(request,response) {
+	let todo = request.body;
+	db.run(queryTInsert, [todo["userId"],todo["title"]], function(err) {
+		if (err) {
+		  return console.log(err.message);
+		}
+		response.send({"id":`${this.lastID}`});
+	});
+	console.log(todo);
+});
+
+// Удаление todo по одному
+app.delete('/todos',jsonParser,function(request,response) {
+	let todo = request.body;
+	db.run(queryTDelete, [todo["id"]], function(err) {
+		if (err) {
+		  return console.log(err.message);
+		}
+	response.send('Deleted');
+	});
+	console.log(todo);
+});
+
+// Редактирование todo: одно поле или все, зависит от параметров
+app.put('/todos',jsonParser,function(request,response) {
+	let todo = request.body;
+	
+	if (typeof todo["title"] == "undefined")
+		db.run(queryTUpdate1, [todo["id"]], function(err) {
+			if (err) {
+			  return console.log(err.message);
+			}
+			response.send('Updated - Switched');
+		});
+	else 
+		db.run(queryTUpdate2, [todo["completed"],todo["title"],todo["id"]], function(err) {
+			if (err) {
+			  return console.log(err.message);
+			}
+			response.send('Updated - Rewrited');
+		});
+	console.log(todo);
 });
 
 app.listen(3001);
